609e6337145a137099e6d1ed6734557c7722423b
danhacker12 <82089973+danhacker12@users.noreply.github.com>
2022-06-24 16:26:18 -0700

    Add files via upload

---
+++ shocolate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace shocolate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите количество денег ");
+            int x = Convert.ToInt32(Console.ReadLine());            
+            int y = 1;            
+            int z = 3;  
+            kolvo_suma123(x, y, z);
+        }        
+        static int  kolvo_suma123(int x, int y, int z)
+        {
+            if (x < y)
+            {
+                return 0;
+            }
+            int xyz = x / y;
+
+            xyz = xyz + (xyz - 1) / (z - 1);
+
+            Console.WriteLine("шоколадки " + xyz);
+
+            return xyz;
+        }      
+    }
+}
